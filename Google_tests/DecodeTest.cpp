844b2f1c1d1ffb315ff2c7b7b3160da43762aa2c
NaschKadse <klein-dominik@web.de>
2022-03-04 21:01:28 +0100

    Second Test

---
+++ Google_tests/DecodeTest.cpp
@@ -0,0 +1,68 @@
+#include "gtest/gtest.h"
+#include "2_Application_Code/decode.h"
+#include "1_Domain_Code/command.h"
+
+
+TEST(DecodeTestSuit, ClrfCorrectString){
+    // Arrange
+    decode decodeTest("00000110000001");
+    command* cmdobject;
+
+    // Act
+    decode::decodedCmd decodedCmdStruct = decodeTest.decodeCommand();
+
+    // Assert
+    EXPECT_EQ(decodedCmdStruct.cmd, "CLRF");
+    EXPECT_EQ(decodedCmdStruct.dBit, -1);
+    EXPECT_EQ(decodedCmdStruct.filepos, 1);
+    EXPECT_EQ(decodedCmdStruct.literal, -1);
+    EXPECT_EQ(typeid(decodedCmdStruct.cmdobject), typeid(cmdobject));
+}
+
+TEST(DecodeTestSuit, BTFSSCorrectString){
+    // Arrange
+    decode decodeTest("01110010000100");
+    command* cmdobject;
+
+    // Act
+    decode::decodedCmd decodedCmdStruct = decodeTest.decodeCommand();
+
+    // Assert
+    EXPECT_EQ(decodedCmdStruct.cmd, "BTFSS");
+    EXPECT_EQ(decodedCmdStruct.dBit, -1);
+    EXPECT_EQ(decodedCmdStruct.filepos, 4);
+    EXPECT_EQ(decodedCmdStruct.literal, 1);
+    EXPECT_EQ(typeid(decodedCmdStruct.cmdobject), typeid(cmdobject));
+}
+
+TEST(DecodeTestSuit, ADDCorrectString){
+    // Arrange
+    decode decodeTest("11111000000010");
+    command* cmdobject;
+
+    // Act
+    decode::decodedCmd decodedCmdStruct = decodeTest.decodeCommand();
+
+    // Assert
+    EXPECT_EQ(decodedCmdStruct.cmd, "ADDLW");
+    EXPECT_EQ(decodedCmdStruct.dBit, -1);
+    EXPECT_EQ(decodedCmdStruct.filepos, -1);
+    EXPECT_EQ(decodedCmdStruct.literal, 2);
+    EXPECT_EQ(typeid(decodedCmdStruct.cmdobject), typeid(cmdobject));
+}
+
+TEST(DecodeTestSuit, InvalidString){
+    // Arrange
+    decode decodeTest("00000000000001");
+    command* cmdobject;
+
+    // Act
+    decode::decodedCmd decodedCmdStruct = decodeTest.decodeCommand();
+
+    // Assert
+    EXPECT_EQ(decodedCmdStruct.cmd, "");
+    EXPECT_EQ(decodedCmdStruct.dBit, -1);
+    EXPECT_EQ(decodedCmdStruct.filepos, -1);
+    EXPECT_EQ(decodedCmdStruct.literal, -1);
+    EXPECT_EQ(typeid(decodedCmdStruct.cmdobject), typeid(cmdobject));
+}
